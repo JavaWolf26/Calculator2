5e20876344b882a8b6e7a760a9090702f7d3d849
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-07-15 23:08:01 +0700

    2

---
+++ src/Calculator1/Calculator2.java
@@ -0,0 +1,57 @@
+package Calculator1;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class Calculator2 {
+
+	public static void main(String[] args) throws java.io.IOException {
+		Scanner keyboard = new Scanner(System.in);
+		int Num1 = 0;
+		int Num2 = 0;
+		String Operation = null;
+		out.print("Vvedite pervoe 4islo ot 0 do 9: ");
+		Num1 = keyboard.nextInt();
+		if (Num1 < 0 || Num1 > 9) {
+			System.out.print("Nevernoe 4islo");
+		} else if (Num1 >= 0 && Num1 < 10) {
+			{
+				out.print("Vvedite vtoroe 4islo ot 0 do 9: ");
+				Num2 = keyboard.nextInt();
+			}
+			if (Num2 < 0 || Num2 > 9) {
+				System.out.print("Nevernoe 4islo!");
+			} else if (Num2 >= 0 && Num2 < 10) {
+				{
+					out.println("Vvedite arifmeti4eskuu operaciu:");
+					out.print("'+', '-', '*', '/' ");
+					Operation = keyboard.next();
+					if (Operation.equals("+")) {
+						int NumRez = Num1 + Num2;
+						out.println(NumRez);
+					} else if (Operation.equals("-")) {
+						int NumRez = Num1 - Num2;
+						out.println(NumRez);
+					} else if (Operation.equals("*")) {
+						int NumRez = Num1 * Num2;
+						out.println(NumRez);
+					} else if (Operation.equals("/")) {
+						try {
+							int NumRez = Num1 / Num2;
+							if (Num1 % Num2 == 0) {
+								System.out.print(NumRez);
+							} else {
+								System.out.print(NumRez + "; ");
+								System.out.print("Ostatoc: " + Num1 % Num2);
+							}
+						} catch (ArithmeticException e) {
+							System.out.println("Delit' na 0 nelza!");
+						}
+					} else
+						System.out.print("Neverna' operacia");
+				}
+			}
+		}
+		keyboard.close();
+	}
+}
