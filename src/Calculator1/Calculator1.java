7c70589fbd083195906cebc3cf2dc9dad6518160
JavaWolf26 <77441182+JavaWolf26@users.noreply.github.com>
2021-07-16 23:44:17 +0700

    1

--- src/Calculator1/Calculator1.java
+++ src/Calculator1/Calculator1.java
@@ -7,8 +7,8 @@ public class Calculator1 {
 
 	public static void main(String[] args) throws java.io.IOException {
 
-		String cifra[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "I", "II", "III", "IV", "V", "VI", "VII",
-				"VIII", "IX" };
+		String cifra1[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		String cifra2[] = { "null", "I", "II", "III", "IV", "V", "VI", "VII",	"VIII", "IX" };
 
 		System.out.println("Vvedite arifmeti4eskoe virazenie,");
 		System.out.println("neobhodimi arabskie 4isla ot 0 do 9");
@@ -24,6 +24,7 @@ public class Calculator1 {
 		int i2;
 		int result;
 
+		if(Arrays.asList(cifra1).contains(str1)) {
 		switch (str1) {
 		case "0":
 		case "1":
@@ -35,17 +36,9 @@ public class Calculator1 {
 		case "7":
 		case "8":
 		case "9":
-		case "I":
-		case "II":
-		case "III":
-		case "IV":
-		case "V":
-		case "VI":
-		case "VII":
-		case "VIII":
-		case "IX":
-			i1 = Arrays.asList(cifra).indexOf(str1);
+			i1 = Arrays.asList(cifra1).indexOf(str1);
 
+			if(Arrays.asList(cifra1).contains(str2)) {
 			switch (str2) {
 			case "0":
 			case "1":
@@ -57,16 +50,7 @@ public class Calculator1 {
 			case "7":
 			case "8":
 			case "9":
-			case "I":
-			case "II":
-			case "III":
-			case "IV":
-			case "V":
-			case "VI":
-			case "VII":
-			case "VIII":
-			case "IX":
-				i2 = Arrays.asList(cifra).indexOf(str2);
+				i2 = Arrays.asList(cifra1).indexOf(str2);
 
 				switch (arif) {
 				case "+":
@@ -84,27 +68,73 @@ public class Calculator1 {
 				case "/":
 					try {
 						result = i1 / i2;
-						if (i1 % i2 == 0) {
-							System.out.print(result);
-						} else {
-							System.out.print(result + "; ");
-							System.out.print("Ostatoc: " + i1 % i2);
-						}
+						if (i1 % i2 == 0) System.out.print(result);
+						else System.out.print(result + "; Ostatoc: " + i1 % i2);
 					} catch (ArithmeticException e) {
 						System.out.println("Delit' na 0 nelza!");
 					}
 					break;
-				default:
-					System.out.println("Neobhodimi arifmeti4eskie operacii +, -, * ili /.");
+				default: System.out.println("Nevernii format arifmeti4eskogo virazenia");
 				}
-				break;
-			default:
-				System.out.println("Neobhodimi arabskie 4isla ot 0 do 9 ili rimskie 4isla ot I do IX");
 			}
-			break;
-		default:
-			System.out.println("Neobhodimi arabskie 4isla ot 0 do 9 ili rimskie 4isla ot I do IX");
+			}else System.out.println("Nevernii format arifmeti4eskogo virazenia");
 		}
+		}else if(Arrays.asList(cifra2).contains(str1)) {
+		switch (str1) {
+		case "null":
+		case "I":
+		case "II":
+		case "III":
+		case "IV":
+		case "V":
+		case "VI":
+		case "VII":
+		case "VIII":
+		case "IX":
+			i1 = Arrays.asList(cifra2).indexOf(str1);
+
+			if(Arrays.asList(cifra2).contains(str2)) {
+			switch (str2) {
+			case "null":
+			case "I":
+			case "II":
+			case "III":
+			case "IV":
+			case "V":
+			case "VI":
+			case "VII":
+			case "VIII":
+			case "IX":
+				i2 = Arrays.asList(cifra2).indexOf(str2);
+
+				switch (arif) {
+				case "+":
+					result = i1 + i2;
+					System.out.print(result);
+					break;
+				case "-":
+					result = i1 - i2;
+					System.out.print(result);
+					break;
+				case "*":
+					result = i1 * i2;
+					System.out.print(result);
+					break;
+				case "/":
+					try {
+						result = i1 / i2;
+						if (i1 % i2 == 0) System.out.print(result);
+						else System.out.print(result + "; Ostatoc: " + i1 % i2);
+					} catch (ArithmeticException e) {
+						System.out.println("Delit' na 0 nelza!");
+					}
+					break;
+				default: System.out.println("Nevernii format arifmeti4eskogo virazenia");
+				}
+			}
+			}else System.out.println("Nevernii format arifmeti4eskogo virazenia");
+		}
+		}else System.out.println("Nevernii format arifmeti4eskogo virazenia");
 		in.close();
 	}
 }
